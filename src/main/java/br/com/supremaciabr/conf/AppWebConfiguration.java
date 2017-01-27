926eb62b22dcfd140c3d8c51dd5d2f89f72b514c
Marcelo <marceloremido@hotmail.com>
2017-01-27 01:42:58 -0200

    make it better

--- src/main/java/br/com/supremaciabr/conf/AppWebConfiguration.java
+++ src/main/java/br/com/supremaciabr/conf/AppWebConfiguration.java
@@ -3,6 +3,8 @@ package br.com.supremaciabr.conf;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import br.com.supremaciabr.controller.index.HomeController;
@@ -10,7 +12,7 @@ import br.com.supremaciabr.controller.index.HomeController;
 
 @EnableWebMvc
 @ComponentScan(basePackageClasses={HomeController.class})
-public class AppWebConfiguration {
+public class AppWebConfiguration extends WebMvcConfigurerAdapter{
 	
 	
 	@Bean
@@ -22,5 +24,10 @@ public class AppWebConfiguration {
 		return resolver;
 	}
 	
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            registry.addResourceHandler("/resources/**")
+                    .addResourceLocations("/resources/");
+    }
 
 }
