f9f46c9b85fc58560a3ffa576d307c7eaa58248f
Marcelo <marceloremido@hotmail.com>
2017-02-03 21:53:57 -0200

    teste com jpa

--- src/main/java/br/com/supremaciabr/conf/JPAConfiguration.java
+++ src/main/java/br/com/supremaciabr/conf/JPAConfiguration.java
@@ -9,20 +9,13 @@
 
 package br.com.supremaciabr.conf;
 
-
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.Properties;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.env.Environment;
-import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
@@ -32,66 +25,80 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
+
 @EnableTransactionManagement
 public class JPAConfiguration {
-	
-	 @Autowired private Environment environment;
 
-	  @Bean
-	   public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
-		 // DriverManagerDataSource dataSource = new DriverManagerDataSource();
-	      LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-	      em.setDataSource(dataSource);
-	      em.setPackagesToScan(new String[] { "br.com.supremaciabr.model" });
-	 
-	      JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-	      em.setJpaVendorAdapter(vendorAdapter);
-	      em.setJpaProperties(additionalProperties());
-	 
-	      return em;
-	   }
-	 
-	   @Bean	   
-	   @Profile("dev")
-	   public DataSource dataSource() throws URISyntaxException{
-		   
-		   
-		   DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		      dataSource.setDriverClassName("org.postgresql.Driver");
-		      URI dbUrl = new URI(environment.getProperty("DATABASE_URL"));
-			  dataSource.setUrl("jdbc:postgresql://" + dbUrl.getHost() + ":" + dbUrl.getPort() + dbUrl.getPath());
-			  dataSource.setUsername(dbUrl.getUserInfo().split(":")[0]);
-			  dataSource.setPassword(dbUrl.getUserInfo().split(":")[1]);
-		      return dataSource;
-		   
-		   
-		   
-		   
-	     /* DriverManagerDataSource dataSource = new DriverManagerDataSource();
-	      dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-	      dataSource.setUrl("jdbc:mysql://localhost:3306/supremacia");
-	      dataSource.setUsername( "root" );
-	      dataSource.setPassword( "root" );
-	      return dataSource;*/
-	   }
-	 
-	   @Bean
-	   public PlatformTransactionManager transactionManager(EntityManagerFactory emf){
-	      JpaTransactionManager transactionManager = new JpaTransactionManager();
-	      transactionManager.setEntityManagerFactory(emf);	 
-	      return transactionManager;
-	   }
-	 
-	   @Bean
-	   public PersistenceExceptionTranslationPostProcessor exceptionTranslation(){
-	      return new PersistenceExceptionTranslationPostProcessor();
-	   }
-	 
-	   Properties additionalProperties() {
-	      Properties properties = new Properties();
-		  properties.setProperty("hibernate.dialect","org.hibernate.dialect.PostgreSQLDialect");
-	      properties.setProperty("hibernate.hbm2ddl.auto", "update");
-	      properties.setProperty("hibernate.show_sql", "false");
-	      return properties;
-	   }
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
+		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+
+		em.setDataSource(dataSource());
+		em.setPackagesToScan(new String[]{
+				"br.com.supremaciabr.model" }
+		);
+		
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		
+		em.setJpaVendorAdapter(vendorAdapter);
+		em.setJpaProperties(additionalProperties());
+	return em;
+	}
+	
+		
+	@Bean
+	@Profile("dev")
+	public DataSource dataSource(){
+	DriverManagerDataSource dataSource =
+	new DriverManagerDataSource();
+	dataSource.setDriverClassName("org.postgresql.Driver");
+    dataSource.setUrl("jdbc:postgresql://localhost:5432/supremacia");
+
+// Mensagens de email 
+// INSERT INTO `user` VALUES('admin','Administrador','$2a$10$EDPGgiRCxt01Jw5gGKdwleQ.j2wG3cRCmGUg87VCEC21ZByh8FNgi');
+// INSERT INTO `user_role` VALUES('admin','ROLE_ADMIN');
+// INSERT INTO `user_role` VALUES('admin','ROLE_DESENVOLVEDOR');
+// INSERT INTO `locomotivos`.`usuario` (`nome`, `email`, `ramal`, `habilitado`, `usuario`, `senha`, `idDepartamento`, `cargo`, `nivel`, `userNovo`) VALUES ('Administrador', 'sisloc@loccoagencia.com.br', '0', '1', 'admin', '@locco1@novosistema#', '4', 'Administrador do Sistema', '2', 'admin');
+// INSERT INTO `criacaoitemstatus` VALUES (1,'Em aberto'),(2,'Fechado'),(3,'Interrompido'),(4,'Excluido do Job'),(5,'Pendente'),(6,'Em Execução'),(7,'Excluído'),(8,'Pendencia Finalizada');	
+// UPDATE `locomotivos`.`criacaostatus` SET `Status`='Abertos' WHERE `idCriacaoStatus`='1';
+// UPDATE `locomotivos`.`criacaostatus` SET `Status`='Fechados' WHERE `idCriacaoStatus`='2';	
+	
+	
+	dataSource.setUsername( "postgres" );
+	dataSource.setPassword( "teste" );
+	return dataSource;
+	}
+	
+	private Properties additionalProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.hbm2ddl.auto","update");
+		properties.setProperty("hibernate.dialect","org.hibernate.dialect.PostgreSQLDialect");
+		properties.setProperty("hibernate.show_sql", "false");
+		properties.setProperty("hibernate.format_sql", "false");
+	//	properties.setProperty("hibernate.generate_statistics", "true");
+		/*properties.setProperty("hibernate.search.default.directory_provider", "filesystem");
+		properties.setProperty("hibernate.search.default.indexBase", "/sysloccOficial/indexes");*/
+		return properties;
+	}
+	
+	
+	
+	@Bean
+	public PlatformTransactionManager transactionManager
+	(EntityManagerFactory emf){
+	JpaTransactionManager transactionManager =
+	new JpaTransactionManager();
+	transactionManager.setEntityManagerFactory(emf);
+	
+	return transactionManager;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
