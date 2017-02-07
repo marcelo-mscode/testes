4e393b1ed7335a4fa49b67b50208cfb3545af9e2
Marcelo <marceloremido@hotmail.com>
2017-02-07 15:38:06 -0200

    first commit

--- src/main/java/br/com/supremaciabr/conf/JPAConfiguration.java
+++ src/main/java/br/com/supremaciabr/conf/JPAConfiguration.java
@@ -18,6 +18,7 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -51,7 +52,7 @@ public class JPAConfiguration {
 	
 		
 	
-  @Bean	   
+  /*@Bean	   
    public DataSource dataSource() throws URISyntaxException{
       DriverManagerDataSource dataSource = new DriverManagerDataSource();
       dataSource.setDriverClassName("org.postgresql.Driver");
@@ -68,17 +69,17 @@ public class JPAConfiguration {
 		properties.setProperty("hibernate.show_sql", "false");
 		properties.setProperty("hibernate.format_sql", "false");
 		return properties;
-	}
+	}*/
 	
-    /*@Bean
+    @Bean
 	@Profile("dev")
 	public DataSource dataSource(){
 	DriverManagerDataSource dataSource =
 	new DriverManagerDataSource();
 	dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-    dataSource.setUrl("jdbc:mysql://localhost:3306/supremacia");
-	dataSource.setUsername( "root" );
-	dataSource.setPassword( "root" );
+    dataSource.setUrl("jdbc:mysql://supremacia.mysql.uhserver.com:3306/supremacia");
+	dataSource.setUsername( "supremacia" );
+	dataSource.setPassword( "@@root@1" );
 	return dataSource;
 	}
 	
@@ -89,8 +90,29 @@ public class JPAConfiguration {
 		properties.setProperty("hibernate.show_sql", "false");
 		properties.setProperty("hibernate.format_sql", "false");
 		return properties;
-	}*/
+	}
+
+	/*	@Bean
+	@Profile("dev")
+	public DataSource dataSource(){
+		DriverManagerDataSource dataSource =
+				new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/supremacia");
+		dataSource.setUsername( "root" );
+		dataSource.setPassword( "root" );
+		return dataSource;
+	}
 	
+	private Properties additionalProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.hbm2ddl.auto","update");
+		properties.setProperty("hibernate.dialect","org.hibernate.dialect.MySQL5Dialect");
+		properties.setProperty("hibernate.show_sql", "false");
+		properties.setProperty("hibernate.format_sql", "false");
+		return properties;
+	}
+*/	
 	
 	
 	
